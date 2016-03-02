ad5919ad524c75b90c4bb9e58f42905f55249423
oliver.naaris <olivernaaris@oliver-laptops-MacBook-Pro.local>
2016-03-02 21:18:32 +0200

    MySql related changes

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,10 +1,31 @@
-include_recipe "selinux::disabled"
+mysql_connection_info = {
+  :host     => '127.0.0.1',
+  :username => 'root',
+  :password => 'mypassword'
+}
 
-mysql_service 'default' do
+mysql2_chef_gem 'exampleloginapp' do
+  action :install
+end
+
+mysql_service 'examplelogin' do
   version '5.7'
   bind_address '0.0.0.0'
   port '3306'
   data_dir '/data'
   initial_root_password 'mypassword'
   action [:create, :start]
+end
+
+mysql_database 'exampleloginapp' do
+  connection mysql_connection_info
+  action :create
+end
+
+
+mysql_database_user 'tutorialuser' do
+  connection mysql_connection_info
+  password   'tutorialmy5ql'
+  host '%'
+  action     :grant
 end
